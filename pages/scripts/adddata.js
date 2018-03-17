3725afc8395bec6279ef2932405d8a1f530d3e7f
pulakhandam lalith sai kishan <saikishan2008@gmail.com>
2018-03-17 14:30:33 +0530

    Update adddata.js

--- pages/scripts/adddata.js
+++ pages/scripts/adddata.js
@@ -23,19 +23,8 @@ $(document).ready(()=>{
             },function(data){
                 console.log(data)
                 data = JSON.parse(data)
-                //window.location.replace("");
+                window.location.replace("../");
             });
         }
     });
-
-    function getBase64Image(imgElem) {
-    // imgElem must be on the same server otherwise a cross-origin error will be thrown "SECURITY_ERR: DOM Exception 18"
-        var canvas = document.createElement("canvas");
-        canvas.width = imgElem.clientWidth;
-        canvas.height = imgElem.clientHeight;
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(imgElem, 0, 0);
-        var dataURL = canvas.toDataURL("image/png");
-        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
 })
