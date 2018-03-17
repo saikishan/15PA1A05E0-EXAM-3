c23a82d20ed5b127d31129f27dada9df3b7735bf
pulakhandam lalith sai kishan <saikishan2008@gmail.com>
2018-03-17 14:28:23 +0530

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -1,11 +1,24 @@
-<script src="/pages/scripts/jqurey.js"></script>
-<script src="/pages/scripts/auth.js"></script>
-<script src="/pages/scripts/login.js"></script>
-<h1>in login php</h1>
-
-<input id="username" type="text" placeholder="username">
-<input id="password" type="password" placeholder="password">
-<input id="btn-login" type="button" value="login">
-<input id="phoneno" type="text" placeholder="username">
-<input id="address" type="text" placeholder="address">
-<input id="btn-register" type="button" value="register">
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="add.css">
+        <script src="/pages/scripts/jqurey.js"></script>
+        <script src="/pages/scripts/auth.js"></script>
+        <script src="/pages/scripts/login.js"></script>
+	</head>
+	<body>
+			<h1>Login/Register</h1>
+			<ul class="nav">
+				<li><a href="../index.php">Home</a></li>
+                <li><a href="login.php">Login/Register</a></li>
+			</ul>
+		<div class="main-content">
+			User Name : <input id="username" type="text" placeholder="username">
+            Password : <input id="password" type="password" placeholder="password">
+            <input id="btn-login" type="button" value="login"><br>
+            Phone Number : <input id="phoneno" type="text" placeholder="number">
+            Address : <input id="address" type="text" placeholder="address">
+            <input id="btn-register" type="button" value="register">
+		</div>
+	</body>
+</html>
