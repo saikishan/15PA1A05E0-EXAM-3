dc44874deea3e416b318dc7ef04a462422189547
pulakhandam lalith sai kishan <saikishan2008@gmail.com>
2018-03-17 14:27:50 +0530

    Update add.php

--- pages/add.php
+++ pages/add.php
@@ -1,8 +1,23 @@
-<script src="/pages/scripts/jqurey.js"></script>
-<script src="/pages/scripts/auth.js"></script>
-<script src="/pages/scripts/adddata.js"></script>
-<input id="myFile" type="file" />
-<img id="img" src="" />
-<input id="price" type="text" placeholder="price">
-<input id="name" type="text" placeholder="name">
-<input id="senddata" type="button" value="senddata">
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="add.css">
+        <script src="/pages/scripts/jqurey.js"></script>
+        <script src="/pages/scripts/auth.js"></script>
+        <script src="/pages/scripts/adddata.js"></script>
+	</head>
+	<body>
+			<h1>Upload</h1>
+			<ul class="nav">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="login.php">Login/Register</a></li>
+			</ul>
+		<div class="main-content">
+			Price :<input id="price" type="text" placeholder="price">
+            Name : <input id="name" type="text" placeholder="name">
+            <input id="myFile" type="file" />
+            <img id="img" src="" />
+            <input id="senddata" type="button" value="senddata">
+		</div>
+	</body>
+</html>
