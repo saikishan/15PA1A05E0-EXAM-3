dc880596ddbc6cddb0e801056339d38d5f8b52d3
pulakhandam lalith sai kishan <saikishan2008@gmail.com>
2018-03-17 14:25:16 +0530

    Update authBack.php

--- services/authBack.php
+++ services/authBack.php
@@ -19,4 +19,27 @@
         }
         echo json_encode($jsondata);
     }
+    if(isset($_POST["register"])){
+        $username=$_POST["username"];
+        $password=$_POST["password"];
+        $address=$_POST["address"];
+        $number=$_POST["number"];
+        $qry = "insert into users(username,password,contactno,address) values('$username','$password','$number','$address')";
+        $result = mysqli_query($conn,$qry);
+        $qry = "select username,password from users where username='$username' and password='$password'";
+        $jsondata = new \stdClass();
+        $result = mysqli_query($conn,$qry);
+        if($result){
+            $row = mysqli_fetch_assoc($result);
+            $cookie = md5($password);
+            $qry2 = "insert into cookie(username,cookie) values('$username','$cookie')";
+            mysqli_query($conn,$qry2);
+            $jsondata->auth = true;
+            $jsondata->cookie = $cookie;
+        }
+        else{
+            $jsondata->auth=false;
+        }
+        echo json_encode($jsondata);
+    }
 ?>
