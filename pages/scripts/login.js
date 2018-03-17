842032c6ed6ff78d9049f712e83ef03def25efd4
pulakhandam lalith sai kishan <saikishan2008@gmail.com>
2018-03-17 14:31:52 +0530

    Update login.js

--- pages/scripts/login.js
+++ pages/scripts/login.js
@@ -20,4 +20,27 @@ $(document).ready(()=>{
             }
         })
     })
+    $("#btn-register").click(()=>{
+        console.log("in the login click");
+        username=$("#username").val();
+        password=$("#password").val();
+        number=$("#phoneno").val();
+        address=$("#address").val();
+        $.post('../services/authBack.php',{
+            register:"login",
+            username:username,
+            password:password,
+            address:address,
+            number:number
+        },(data)=>{
+            data=JSON.parse(data)
+            console.log(data)
+            if(data.auth){
+                window.sessionStorage.setItem("olxwebcookie",data.cookie);
+                console.log(window.sessionStorage.getItem("olxwebcookie"))
+                console.log("the add.php")
+                window.location.replace("add.php")
+            }
+        })
+    })
 })
